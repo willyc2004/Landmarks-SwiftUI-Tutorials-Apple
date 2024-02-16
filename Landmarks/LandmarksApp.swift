26d29ca7562ba66682e2f491fa3affac182ed562
willyc2004 <133841163+willyc2004@users.noreply.github.com>
2024-02-16 14:25:53 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by MacBook Pro on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
